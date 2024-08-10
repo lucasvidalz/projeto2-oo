6ea92f9b16882067a36d33c47bade24ba3925973
lucasvidal <lucasvidalk9@gmail.com>
2024-08-10 20:39:02 -0300

    projeto finalizado 20:38 - 10/08/2024

--- controllers/form_controller.py
+++ controllers/form_controller.py
@@ -1,9 +1,9 @@
 from tkinter import messagebox
 from models.client_management_system import ClientManagementSystem
-#classe de controle da entrada de dados, ela faz as verificações para a ClientManagement
+
 class FormController:
     def __init__(self):
-        self.client_system = ClientManagementSystem()
+        self.__client_system = ClientManagementSystem()
 
     def save_client(self, name, contact, age, address, gender, obs):
         if not name or not contact or not age:
@@ -19,11 +19,11 @@ class FormController:
             messagebox.showerror("Erro", "Por favor, insira uma idade válida.")
             return
 
-        self.client_system.set_name(name)
-        self.client_system.set_contact(contact)
-        self.client_system.set_age(age)
-        self.client_system.set_address(address)
-        self.client_system.set_gender(gender)
-        self.client_system.set_obs(obs)
+        self.__client_system.set_name(name)
+        self.__client_system.set_contact(contact)
+        self.__client_system.set_age(age)
+        self.__client_system.set_address(address)
+        self.__client_system.set_gender(gender)
+        self.__client_system.set_obs(obs)
 
-        self.client_system.save_data()
+        self.__client_system.save_data()
--- reports/generate_report.py
+++ reports/generate_report.py
@@ -1,5 +1,7 @@
+from abc import ABC, abstractmethod
 # classe base
-class ReportGenerator:
+class ReportGenerator(ABC):
     # metodo polimorifico - vai ser usado para gerar diferentes tipos de arquivos
+    @abstractmethod
     def generate_report(self):
         raise NotImplementedError("Subclasses devem implementar este método.")
